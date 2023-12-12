32a4d42832bba73a0a0c75083f5b8c99be033bec
Prodan Robert-Alexandru <prodanrobert123@gmail.com>
2023-12-12 23:48:27 +0200

    back-up

--- EmailCentral/myapp/management/commands/fetch_emails.py
+++ EmailCentral/myapp/management/commands/fetch_emails.py
@@ -1,26 +1,23 @@
-# your_app/management/commands/fetch_emails.py
 from django.core.management.base import BaseCommand
-from myapp.models import Email  # Import your Email model
+from django.utils import timezone
+from myapp.models import Email
 import imaplib
 import email
-import csv
 import yaml
 
 class Command(BaseCommand):
-    help = 'Fetch emails from Gmail and save information to the database'
+    help = 'Fetch emails from Gmail and update the database'
 
     def handle(self, *args, **options):
-        # Your script to fetch emails goes here
-        # Importing libraries
-        with open(r"C:\Users\ricir\OneDrive\Documents\GitHub\EmailCentral\EmailCentral\myapp\management\commands\credentials.yml") as f:
-            content = f.read()
-
-        my_credentials = yaml.load(content, Loader=yaml.FullLoader)
-        user, password = my_credentials["user"], my_credentials["password"]
-        imap_url = 'imap.gmail.com'
-        my_mail = imaplib.IMAP4_SSL(imap_url)
-
         try:
+            with open(r"C:\Users\ricir\OneDrive\Documents\GitHub\EmailCentral\EmailCentral\myapp\management\commands\credentials.yml") as f:
+                content = f.read()
+
+            my_credentials = yaml.load(content, Loader=yaml.FullLoader)
+            user, password = my_credentials["user"], my_credentials["password"]
+            imap_url = 'imap.gmail.com'
+            my_mail = imaplib.IMAP4_SSL(imap_url)
+
             # Log in using your credentials
             my_mail.login(user, password)
             # Select the Inbox to fetch messages
@@ -38,33 +35,32 @@ class Command(BaseCommand):
                 typ, data = my_mail.fetch(num, '(RFC822)')
                 msgs.append(data)
 
-            # Iterate through messages and save information to the database
+            # Iterate through messages and store in the database
             for msg in msgs[::-1]:
                 for response_part in msg:
                     if type(response_part) is tuple:
                         my_msg = email.message_from_bytes(response_part[1])
-                        subject = my_msg['subject']
                         sender = my_msg['from']
-                        body = ''
+                        subject = my_msg['subject']
+                        content = ""
+
+                        # Extract content if available
                         for part in my_msg.walk():
                             if part.get_content_type() == 'text/plain':
-                                body = part.get_payload()
+                                content = part.get_payload()
 
-                        # Create and save Email object to the database
-                        email_obj = Email(
-                            sender=sender,
-                            subject=subject,
-                            content=body,
-                            source='gmail'  # Set the source to 'gmail'
-                            # attachments will be None since emails don't have attachments
-                        )
-                        email_obj.save()
-
-            self.stdout.write(self.style.SUCCESS('Email information saved to the database.'))
+                        # Check if the email already exists
+                        if not Email.objects.filter(sender=sender, subject=subject, content=content).exists():
+                            # Email does not exist, create a new entry
+                            email_obj = Email(sender=sender, subject=subject, content=content, source='Gmail', received_at=timezone.now())
+                            email_obj.save()
 
         except Exception as e:
             self.stdout.write(self.style.ERROR(f'Error: {e}'))
 
         finally:
-            # Always close the connection
-            my_mail.logout()
+            # Close the connection if it was opened successfully
+            if 'my_mail' in locals():
+                my_mail.logout()
+
+        self.stdout.write(self.style.SUCCESS('Emails fetched and stored in the database successfully.'))
--- EmailCentral/myapp/models.py
+++ EmailCentral/myapp/models.py
@@ -1,5 +1,6 @@
 # models.py in your app
 from django.db import models
+from django.utils import timezone
 
 class Email(models.Model):
     sender = models.CharField(max_length=255)
@@ -7,7 +8,11 @@ class Email(models.Model):
     content = models.TextField()
     attachments = models.FileField(upload_to='attachments/', blank=True, null=True)
     source = models.CharField(max_length=50, default='unknown')
+    received_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)  # Set a default value
+
     def __str__(self):
         return self.subject
+
     def filename(self):
         return os.path.basename(self.attachments.name)
