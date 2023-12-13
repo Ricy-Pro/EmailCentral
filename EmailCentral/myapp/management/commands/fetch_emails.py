fba0da6df0387208c91e8cd9f50f4a1af94a64d3
Prodan Robert-Alexandru <prodanrobert123@gmail.com>
2023-12-13 12:47:21 +0200

    Good progress

--- EmailCentral/myapp/management/commands/fetch_emails.py
+++ EmailCentral/myapp/management/commands/fetch_emails.py
@@ -1,12 +1,15 @@
+from email import policy
+from email.parser import BytesParser
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from myapp.models import Email, Attachment
-
 
 import imaplib
 import email
 import yaml
 import pytz
+from email import policy
+from email.parser import BytesParser
 
 class Command(BaseCommand):
     help = 'Fetch emails from Gmail and update the database'
@@ -47,7 +50,12 @@ class Command(BaseCommand):
             for msg in msgs[::-1]:
                 for response_part in msg:
                     if type(response_part) is tuple:
-                        my_msg = email.message_from_bytes(response_part[1])
+                        msg_data = response_part[1]
+                        msg_text = msg_data.decode('utf-8', errors='replace')
+                        msg_bytes = msg_text.encode('utf-8')
+                        
+                        my_msg = BytesParser(policy=policy.default).parsebytes(msg_bytes)
+
                         sender = my_msg['from']
                         subject = my_msg['subject']
                         content = ""
@@ -85,15 +93,28 @@ class Command(BaseCommand):
                                     # Handle parsing errors
                                     self.stdout.write(self.style.ERROR(f'Error parsing date: {e}'))
                                     continue  # Skip this email if date parsing fails
+                            # Try parsing with (UTC) suffix
+                            try:
+                                received_at_utc = timezone.datetime.strptime(date_header, '%a, %d %b %Y %H:%M:%S %z')
+                            except ValueError:
+                                # Handle parsing errors
+                                self.stdout.write(self.style.ERROR(f'Error parsing date: {e}'))
+                                continue  # Skip this email if date parsing fails
 
                             # Check if the timezone is present in the date header
                             if received_at_utc.tzinfo is None:
                                 # If not, assume GMT
                                 received_at_utc = received_at_utc.replace(tzinfo=pytz.timezone('GMT'))
 
+                            # Add 2 hours to the received time
+                            received_at_utc += timezone.timedelta(hours=2)
+
                             # Convert to Romanian timezone
                             romanian_timezone = pytz.timezone('Europe/Bucharest')
                             received_at_romanian = received_at_utc.astimezone(romanian_timezone)
+
+
+
 
                             email_obj = Email(sender=sender, subject=subject, content=content, source='Gmail', received_at=received_at_romanian)
                             email_obj.save()
